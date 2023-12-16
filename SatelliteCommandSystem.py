54275098a81cf3b494e12cd6ca0b6b395788776e
Vinod Ramesh Patgar <97332885+VinodPatgar02@users.noreply.github.com>
2023-12-16 22:39:34 +0530

    Update SatelliteCommandSystem.py

--- SatelliteCommandSystem.py
+++ SatelliteCommandSystem.py
@@ -1,6 +1,6 @@
 import logging
+from enum import Enum
 
-# Define custom exceptions for specific error cases.
 class InvalidDirectionError(Exception):
     pass
 
@@ -16,41 +16,39 @@ class InactiveSolarPanelsError(Exception):
 class TransientError(Exception):
     pass
 
-# Create a decorator for handling transient errors gracefully.
+class Direction(Enum):
+    NORTH = "North"
+    SOUTH = "South"
+    EAST = "East"
+    WEST = "West"
+
 def transient_error_handler(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except TransientError as e:
-            logging.warning(f"Transient Error: {e}")  # Log transient errors.
-            print(f"Transient Error: {e}")  # Print transient errors.
+            logging.exception(f"Transient Error: {e}")
+            print(f"Transient Error: {e}")
     return wrapper
 
-# Define the Satellite class to represent the satellite and its behavior.
 class Satellite:
     def __init__(self):
-        # Initialize the satellite's initial state.
-        self.orientation = "North"
+        self.orientation = Direction.NORTH
         self.solar_panels = "Inactive"
         self.data_collected = 0
 
-    # Method to rotate the satellite's orientation.
     def rotate(self, direction):
-        valid_directions = ["North", "South", "East", "West"]
-        if direction in valid_directions:
-            self.orientation = direction
-            logging.info(f"Rotated to {direction}.")  # Log the rotation.
-        else:
+        if direction not in Direction._value2member_map_:
             raise InvalidDirectionError("Invalid direction. Use 'North', 'South', 'East', or 'West'.")
+        self.orientation = direction
+        logging.info(f"Rotated to {direction}.")
 
-    # Method to set the solar panel status.
     def set_solar_panels(self, status):
         if self.solar_panels == status:
             raise TransientError(f'Solar panels are already {status.lower()}.')
         self.solar_panels = status
-        logging.info(f'Solar panels {status.lower()}ed.')  # Log the change in solar panel status.
+        logging.info(f'Solar panels {status.lower()}ed.')
 
-    # Methods with transient error handling decorators to activate, deactivate, and collect data.
     @transient_error_handler
     def activate_panels(self):
         self.set_solar_panels("Active")
@@ -63,38 +61,34 @@ class Satellite:
     def collect_data(self):
         if self.solar_panels == "Active":
             self.data_collected += 10
-            logging.info("Data collected.")  # Log data collection.
+            logging.info("Data collected.")
         else:
             raise InactiveSolarPanelsError("Cannot collect data with inactive solar panels.")
 
-# The main function where the program execution begins.
-def main():
-    # Configure logging to log events to a file.
-    logging.basicConfig(filename='satellite.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    
-    # Create an instance of the Satellite class.
-    satellite = Satellite()
-    logging.info("Satellite initialized.")  # Log satellite initialization.
-    
-    # Display the initial state of the satellite.
-    print("Initial State:")
-    print(f"Orientation: {satellite.orientation}")
+def print_satellite_state(satellite):
+    print("Current State:")
+    print(f"Orientation: {satellite.orientation.value}")
     print(f"Solar Panels: {satellite.solar_panels}")
     print(f"Data Collected: {satellite.data_collected}")
-    print()
+
+def main():
+    logging.basicConfig(filename='satellite.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    
+    satellite = Satellite()
+    logging.info("Satellite initialized.")
+    
+    print_satellite_state(satellite)
 
     while True:
-        # Accept user commands to interact with the satellite.
         command = input("Enter a command (rotate, activatePanels, deactivatePanels, collectData, status, or exit): ")
         
         if command == "exit":
             break
 
         try:
-            # Execute user commands and log relevant events.
             if command == "rotate":
                 direction = input("Enter a direction (North, South, East, West): ")
-                satellite.rotate(direction)
+                satellite.rotate(Direction[direction])
             elif command == "activatePanels":
                 satellite.activate_panels()
             elif command == "deactivatePanels":
@@ -102,18 +96,13 @@ def main():
             elif command == "collectData":
                 satellite.collect_data()
             elif command == "status":
-                # Display the current status when requested.
-                print("Current State:")
-                print(f"Orientation: {satellite.orientation}")
-                print(f"Solar Panels: {satellite.solar_panels}")
-                print(f"Data Collected: {satellite.data_collected}")
+                print_satellite_state(satellite)
             else:
                 print("Invalid command. Please try again.")
                 continue
         except (InvalidDirectionError, SolarPanelsAlreadyActiveError, SolarPanelsAlreadyInactiveError, InactiveSolarPanelsError) as e:
             print(f"Error: {e}")
-            logging.error(f"Error: {e}")  # Log errors.
+            logging.error(f"Error: {e}")
 
-# Execute the main function if this script is run.
 if __name__ == "__main__":
     main()
